3da17acc357af2dc74f20609b92042122e83cb7d
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-01-22 14:07:09 -0300

    Entrada na sala bugs corrigidos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let respostaDoServidor;
+let objetoNome;
 function tratarSucessoNoStatus(resposta){
     console.log("usuário conectado");
     console.log(resposta.status);
@@ -8,8 +8,8 @@ function tratarErroNoStatus(erro){
     alert("Erro na verificação do status do usuário");
     entrarNaSala();
 }
-function verificarStatusDoUsuario(nome){
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",nome);
+function verificarStatusDoUsuario(){
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",objetoNome);
     promise.then(tratarSucessoNoStatus);
     promise.catch(tratarErroNoStatus);
 }
@@ -17,28 +17,25 @@ function verificarStatusDoUsuario(nome){
 function tratarSucessoNoCadastro(resposta){
     console.log(resposta.status);
     alert('Usuário cadastrado com sucesso!');
-    return resposta.status;    
+    setInterval(verificarStatusDoUsuario, 5000);
 }
 function tratarErroNoCadastro(erro){
     console.log(erro.response.status);
     alert("Esse nome já está em uso. Por favor, digite outro nome");
     entrarNaSala(); 
 }
-function cadastrarUsuario(nome){
+function cadastrarUsuario(){
     //Enviar o nome do usuário para o servidor
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nome);
-    respostaDoServidor = promise.then(tratarSucessoNoCadastro);
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objetoNome);
+    promise.then(tratarSucessoNoCadastro);
     promise.catch(tratarErroNoCadastro);
 }
 function entrarNaSala(){
 //Ao entrar no site, perguntar o nome do usuário via prompt    
     const nomeDoUsuario = prompt("Qual é o seu nome?");
-    const objetoNome = {
+    objetoNome = {
         name: nomeDoUsuario,
     };
-    cadastrarUsuario(objetoNome);
-//if(respostaDoServidor === 200 || respostaDoServidor === 201){
-    setInterval(verificarStatusDoUsuario, 5000, objetoNome);
+    cadastrarUsuario();
 }
-//}
 entrarNaSala();
